f90300786ae8bd55f40d268fd50b5c4ca37f1b31
Paul Maidment <paul@paulmaidment.co.uk>
2019-01-26 10:22:16 +0000

    Building character model

--- WhoLivesInThisHouse/Character.cs
+++ WhoLivesInThisHouse/Character.cs
@@ -5,11 +5,21 @@ namespace WhoLivesInThisHouse
 {
     public class Character
     {
+        private String name;
         private List<Tag> likeTags;
         private List<Tag> dislikeTags;
 
-        public Character(List<Tag> likeTags, List<Tag> dislikeTags)
+        public String Name
         {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public Character(String name, List<Tag> likeTags, List<Tag> dislikeTags)
+        {
+            this.name = name;
             this.likeTags = likeTags;
             this.dislikeTags = dislikeTags;
         }
@@ -28,6 +38,45 @@ namespace WhoLivesInThisHouse
             {
                 return this.dislikeTags;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (!(obj is Character))
+            {
+                return false;
+            }
+
+            Character other = (Character) obj;
+
+            if (!Name.Equals(other.Name))
+            {
+                return false;
+            }
+
+            if(!LikeTags.Equals(other.LikeTags))
+            {
+                return false;
+            }
+
+            if(!DislikeTags.Equals(other.DislikeTags))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() 
+                + 31 * this.LikeTags.GetHashCode() 
+                + 31 * this.DislikeTags.GetHashCode();
         }
     }
 }
--- WhoLivesInThisHouseTest/CharacterTest.cs
+++ WhoLivesInThisHouseTest/CharacterTest.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using WhoLivesInThisHouse;
+
 namespace WhoLivesInThisHouseTest
 {
     [TestFixture()]
@@ -8,7 +11,58 @@ namespace WhoLivesInThisHouseTest
         [Test()]
         public void it_should_be_constructed_with_likes_and_dislikes_as_tags()
         {
+            List<Tag> likeTags = new List<Tag> { new Tag("tag1"), new Tag("tag2")};
+            List<Tag> dislikeTags = new List<Tag> { new Tag("tag3"), new Tag("tag4")};
+            Character character = new Character(
+                "Bob",
+                likeTags,
+                dislikeTags
+            );
+            Assert.AreEqual("Bob", character.Name);
+            Assert.AreEqual(2, character.LikeTags.Count);
+            Assert.AreEqual("tag1", character.LikeTags[0].GetName());
+            Assert.AreEqual("tag2", character.LikeTags[1].GetName());
+            Assert.AreEqual(2, character.DislikeTags.Count);
+            Assert.AreEqual("tag3", character.DislikeTags[0].GetName());
+            Assert.AreEqual("tag4", character.DislikeTags[1].GetName());
+        }
 
+        [Test()]
+        public void it_should_be_considered_equal_to_another_character_if_the_attributes_are_identical()
+        {
+            List<Tag> likeTags = new List<Tag> { new Tag("tag1"), new Tag("tag2") };
+            List<Tag> dislikeTags = new List<Tag> { new Tag("tag3"), new Tag("tag4") };
+
+            Character character1 = new Character(
+                "Bob",
+                likeTags,
+                dislikeTags
+            );
+
+            Character character2 = new Character(
+                "Bob",
+                likeTags,
+                dislikeTags
+            );
+
+            Assert.AreEqual(character1, character2);
+
+            List<Tag> moreTags = new List<Tag> { new Tag("tag4"), new Tag("tag5") };
+            Character character3 = new Character(
+                "Bob",
+                moreTags,
+                dislikeTags
+            );
+
+            Assert.AreNotEqual(character2, character3);
+
+            Character character4 = new Character(
+                "Bob",
+                likeTags,
+                moreTags
+            );
+
+            Assert.AreNotEqual(character2, character4);
         }
     }
 }
