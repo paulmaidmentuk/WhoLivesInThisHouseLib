06197d3279ac37188242784df26805a01449d91b
Paul Maidment <paul@paulmaidment.co.uk>
2019-01-27 13:37:06 +0000

    Character list API client

---
+++ WhoLivesInThisHouse/CharacterListApiCaller.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WhoLivesInThisHouse
+{
+    public class CharacterListApiCaller
+    {
+        private String serverBaseUrl;
+        private CharacterListDeserializer deserializer;
+
+        public CharacterListApiCaller(String serverBaseUrl)
+        {
+            this.serverBaseUrl = serverBaseUrl;
+            this.deserializer = new CharacterListDeserializer();
+        }
+
+        public List<Character> GetCharacters()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverBaseUrl + "/api/characters");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string jsonResponse = reader.ReadToEnd();
+            return deserializer.Deserialize(jsonResponse);
+        }
+    }
+}
